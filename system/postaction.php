15faff09c04a934f502b14700578c98a645b9cbe
jadewizard <jadewizard@yandex.ru>
2015-10-24 10:13:41 +0300

    обработка постов

---
+++ system/postaction.php
@@ -0,0 +1,49 @@
+<?php
+/*
+* File: postaction.php
+* Functions: Работа с постами.
+* Author: jadewizard
+* Buryakov.su
+*/
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+$opt = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+$pdo = new PDO("mysql:dbname=blog;host=localhost;charset=utf8","root","211996dima",$opt);
+
+/*
+===================== ПОЯСНЕНИЕ ==========================
+* Если текущее действие ($_POST['action']) - это
+* добавление (add), то выполняем функцию добавления в БД
+* Если текущее действие обновление (update), то просто
+* получаем ID, только что доавбленного поста (MAX ID в БД)
+* и обновляем строки с этим ID.
+==========================================================
+*/
+if($_POST['action'] == 'add')
+{
+    $title = $_POST['postTitle'];
+    $text = $_POST['postText'];
+
+    $addQuery = $pdo->prepare("INSERT INTO articles (title,content) VALUES (:title, :content)");
+    $addQuery->execute(array("title" => $title, "content" => $text));
+}
+elseif ($_POST['action'] == "update")
+{
+    $title = $_POST['postTitle'];
+    $text = $_POST['postText'];
+
+    $lastIdArray = $pdo->query("SELECT MAX(id) FROM articles")->fetchAll(PDO::FETCH_ASSOC)[0];
+    $lastId = $lastIdArray['MAX(id)'];
+
+    $addQuery = $pdo->prepare("UPDATE articles SET title = :title, content = :content WHERE id = :lastId");
+    $addQuery->execute(array("title" => $title, "content" => $text, "lastId" => $lastId));
+}
+elseif($_POST['action'] == "delete")
+{
+    $id = $_POST['id'];
+    $deleteQuery = $pdo->prepare("DELETE FROM articles WHERE id = ?");
+    $deleteQuery->execute(array($id));
+}
