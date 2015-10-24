5738ed1e5a63315f8d6e494fda2477dc9063afa1
jadewizard <jadewizard@yandex.ru>
2015-10-24 11:45:14 +0300

    Обработка редактирования поста

--- system/actionhandler.php
+++ system/actionhandler.php
@@ -5,3 +5,17 @@
 * Author: jadewizard
 * Buryakov.su
 */
+
+if($_GET['p'] == "adminpanel" && isset($_GET['a']))
+{
+    if(isset($_GET['a']) == "edit" && isset($_GET['id']))
+    {
+        /*
+        Если пользователь находится на странице
+        с редактированьем поста, то берём ID этого поста
+        и получаем из БД его данные.
+        */
+        $id = $_GET['id'];
+        $post->getPostById($id);
+    }
+}
