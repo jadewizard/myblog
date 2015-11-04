ac77bfb4299503f8c919d77dbe63825461e164d6
jadewizard <jadewizard@yandex.ru>
2015-11-04 19:39:53 +0300

    + файлы и Бд

--- system/engine.php
+++ system/engine.php
@@ -16,15 +16,23 @@ Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('templates'); //Путь к шаблону
 $twig = new Twig_Environment($loader); //Инициализируем шаблонизатор
 
+require_once 'class/pagination.class.php';
+$pgManager = new pagination();
+// Пагинация
+
 require_once 'class/post.class.php';
-$post = new Post();
+$post = new post();
 //Работа с постами на сайте
 
 require_once 'class/app.class.php';
 $application = new application();
 //Различные функции сайта
 
+
+$pgManager->getLinks();
+
 $application->getBreadCrumbs();
+// Хлебные крошки
 
 require_once 'actionhandler.php';
 //Обработка пользовательских действий
